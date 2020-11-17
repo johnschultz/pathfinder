a0afa4eb4e5dae55f3153eff97d99a69297b3d16
Louis Cadet <cadet.louis@gmail.com>
2020-11-17 15:15:09 +0000

    Update for new Pochven region https://github.com/exodus4d/pathfinder/issues/1001

--- app/Controller/Api/Rest/Route.php
+++ app/Controller/Api/Rest/Route.php
@@ -665,7 +665,8 @@ class Route extends AbstractRestController {
             // search route -------------------------------------------------------------------------------------------
             $options = [
                 'flag' => $filterData['flag'],
-                'connections' => $connections
+		'connections' => $connections,
+		'avoid' => [ 30000021, 30000157, 30000192, 30000206, 30001372, 30001381, 30001413, 30001445, 30002079, 30002225, 30002411, 30002652, 30002702, 30002737, 30002770, 30002797, 30003046, 30003495, 30003504, 30005005, 30005029, 30010141, 30020141, 30031392, 30040141, 30045328, 30045329 ]
             ];
 
             $result = $this->getF3()->ccpClient()->send('getRoute', $systemFromId, $systemToId, $options);
